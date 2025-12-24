98ff1f66b4e9d0f7adc7728268766f253462a584
damer <boronenko222@gmail.com>
2025-12-24 23:19:57 +1000

    Документирование с использованием Doxygen

--- operations.cpp
+++ operations.cpp
@@ -1,16 +1,38 @@
+/**
+ * @file operations.cpp
+ * @brief Реализация функций для операций над массивом чисел.
+ *
+ * Этот файл содержит реализацию функций для сортировки, реверса и перестановки элементов массива.
+ */
+
 #include "operations.h"
 #include <algorithm>
 
+ /**
+  * @brief Сортирует массив чисел по возрастанию.
+  *
+  * @param data Вектор целых чисел для сортировки.
+  */
 extern "C" void sort(vector<int>& data) {
-    sort(data.begin(), data.end());
+    std::sort(data.begin(), data.end());
 }
 
+/**
+ * @brief Разворачивает массив чисел.
+ *
+ * @param data Вектор целых чисел для реверса.
+ */
 extern "C" void reverse(vector<int>& data) {
-    reverse(data.begin(), data.end());
+    std::reverse(data.begin(), data.end());
 }
 
+/**
+ * @brief Переставляет чётные и нечётные элементы массива.
+ *
+ * @param data Вектор целых чисел для перестановки.
+ */
 extern "C" void swap_even_odd(vector<int>& data) {
     for (size_t i = 0; i + 1 < data.size(); i += 2) {
-        swap(data[i], data[i + 1]);
+        std::swap(data[i], data[i + 1]);
     }
 }
--- operations.h
+++ operations.h
@@ -1,3 +1,10 @@
+/**
+ * @file operations.h
+ * @brief Заголовочный файл с определением функций для операций над массивом чисел.
+ *
+ * Этот файл содержит объявления функций для сортировки, реверса и перестановки элементов массива.
+ */
+
 #ifndef OPERATIONS_H
 #define OPERATIONS_H
 
@@ -5,8 +12,25 @@
 
 using namespace std;
 
+/**
+ * @brief Сортирует массив чисел по возрастанию.
+ *
+ * @param data Вектор целых чисел для сортировки.
+ */
 extern "C" void sort(vector<int>& data);
+
+/**
+ * @brief Разворачивает массив чисел.
+ *
+ * @param data Вектор целых чисел для реверса.
+ */
 extern "C" void reverse(vector<int>& data);
+
+/**
+ * @brief Переставляет чётные и нечётные элементы массива.
+ *
+ * @param data Вектор целых чисел для перестановки.
+ */
 extern "C" void swap_even_odd(vector<int>& data);
 
-#endif
+#endif // OPERATIONS_H
